6233e68baca989832dbebbf1acf0f7d63d12ceb0
GustavoCorona <corona_gustavo@student.smc.com>
2025-07-13 15:30:30 -0700

    Updated myWeek data and finalized predictions

---
+++ activityTracker.js
@@ -0,0 +1,71 @@
+// Gustavo Corona
+// activityTracker.js made July 13, 2025
+
+const myWeek = [
+  {
+    day: "Monday",
+    activity: "Gym", 
+    category: "physical",
+    hoursSpent: 1.0,
+    enjoyment: 4,
+    timeOfDay: "morning"
+  },
+  {
+    day: "Tuesday",
+    activity: "Video games", 
+    category: "entertainment",
+    hoursSpent: 2.0,
+    enjoyment: 8,
+    timeOfDay: "night"
+  },
+  {
+    day: "Wednesday",
+    activity: "Hanging out with friends", 
+    category: "social",
+    hoursSpent: 3.0,
+    enjoyment: 9,
+    timeOfDay: "evening"
+  },
+  {
+    day: "Thursday",
+    activity: "Reading", 
+    category: "leisure",
+    hoursSpent: 1.0,
+    enjoyment: 7,
+    timeOfDay: "night"
+  },
+  {
+    day: "Friday",
+    activity: "Watch Youtube", 
+    category: "entertainment",
+    hoursSpent: 2.5,
+    enjoyment: 8,
+    timeOfDay: "evening"
+  },
+  {
+    day: "Saturday",
+    activity: "Homework", 
+    category: "learning",
+    hoursSpent: 2.0,
+    enjoyment: 6,
+    timeOfDay: "afternoon"
+  },
+  {
+    day: "Sunday",
+    activity: "Homework", 
+    category: "learning",
+    hoursSpent: 5.5,
+    enjoyment: 5,
+    timeOfDay: "afternoon"
+  }
+];
+
+// PREDICTIONS:
+// * Which activity will have the highest enjoyment? *
+//      -Hanging out with friends will have the highest enjoyment.
+
+// * What category will dominate your week? *
+//      - I expect 'learning' or 'entertainment' to be the most frequent category.
+
+// * What patterns might exist around time of day? * 
+//      - I anticipate 'evening' activities will have the most enjoyment, and 'afternoon' activities will have higher hours spent.
